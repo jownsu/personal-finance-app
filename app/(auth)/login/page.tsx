c8db93b7de18913ee76b95de0cb6e1dec08b76dd
Jownsu <jhonesdigno666@gmail.com>
2024-10-25 10:03:17 +0800

    Form auth validations

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,33 +1,82 @@
+"use client";
+
 /* REACT */
 import Link from "next/link";
+import { useState } from "react";
 
 /* COMPONENTS */
 import Button from "@/app/_components/ui/Button";
 import { Input } from "@/app/_components/ui/Input";
 import { Label } from "@/app/_components/ui/Label";
 
+/* PLUGINS */
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+/* SCHEMA */
+import { login_schema, LoginSchema } from "@/app/_schema/auth.schema";
+
 /* ICONS */
 import IconShowPassword from "@/public/images/icon-show-password.svg";
 
 const LoginPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<LoginSchema>({
+        resolver: zodResolver(login_schema)
+    });
+
+    const [show_password, setShowPassword] = useState(true);
+
+    const onSubmit: SubmitHandler<LoginSchema> = (data) => {
+        console.log(data);
+    };
+
     return (
-        <form className="flex w-full max-w-[56rem] flex-col gap-[3.2rem] rounded-[1.2rem] bg-white px-[2rem] py-[2.4rem] md:p-[3.2rem]">
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex w-full max-w-[56rem] flex-col gap-[3.2rem] rounded-[1.2rem] bg-white px-[2rem] py-[2.4rem] md:p-[3.2rem]"
+        >
             <h1 className="text-preset_1">Login</h1>
             <div className="grid gap-[1.6rem]">
                 <div className="grid gap-[.4rem]">
                     <Label htmlFor="email">Email</Label>
-                    <Input type="email" id="email" />
+                    <Input
+                        className="text-preset_4"
+                        type="email"
+                        id="email"
+                        {...register("email")}
+                    />
+                    {errors.email && (
+                        <span className="text-end text-preset_5 text-grey-500">
+                            {errors.email.message}
+                        </span>
+                    )}
                 </div>
                 <div className="relative grid gap-[.4rem]">
                     <Label htmlFor="password">Password</Label>
-                    <Input type="password" id="password" />
+                    <Input
+                        className="text-preset_4"
+                        type={show_password ? "password" : "text"}
+                        id="password"
+                        {...register("password")}
+                    />
                     <button
                         type="button"
                         aria-label="Show password"
-                        className="absolute right-[2rem] top-[50%]"
+                        className="absolute right-[2rem] top-[3rem]"
+                        onClick={() => setShowPassword(!show_password)}
                     >
                         <IconShowPassword />
                     </button>
+
+                    {errors.password && (
+                        <span className="text-right text-preset_5 text-grey-500">
+                            {errors.password.message}
+                        </span>
+                    )}
                 </div>
             </div>
 
