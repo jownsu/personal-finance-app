11120c32a0a59e9330dcdb07c90730e762293f59
Jownsu <jhonesdigno666@gmail.com>
2024-10-26 13:41:18 +0800

    Minimize/Maximize of Sidebar

--- app/_components/NavigationBar.tsx
+++ app/_components/NavigationBar.tsx
@@ -10,7 +10,8 @@ import OverviewIcon from "@/public/images/icon-nav-overview.svg";
 import PotIcon from "@/public/images/icon-nav-pots.svg";
 import BillsIcon from "@/public/images/icon-nav-recurring-bills.svg";
 import TransactionIcon from "@/public/images/icon-nav-transactions.svg";
-import Logo from "@/public/images/logo-large.svg";
+import LargeLogo from "@/public/images/logo-large.svg";
+import SmallLogo from "@/public/images/logo-small.svg";
 import BackIcon from "@/public/images/icon-minimize-menu.svg";
 
 /* HELPERS */
@@ -55,9 +56,32 @@ const NavigationBar = () => {
     const [is_open, setOpen] = useState(true);
 
     return (
-        <nav className="mt-auto h-[5.2rem] rounded-t-[.8rem] bg-grey-900 px-[1.6rem] pt-[.8rem] md:h-[7.4rem] md:px-[4rem] lg:mt-0 lg:flex lg:h-auto lg:basis-[30rem] lg:flex-col lg:gap-[2.4rem] lg:rounded-r-[1.6rem] lg:rounded-tl-none lg:px-0 lg:pb-[5.8rem] lg:pr-[2.4rem] lg:pt-0">
-            <div className="hidden px-[3.2rem] py-[4rem] lg:block">
-                <Logo className="h-[2.2rem] w-[12.1rem]" />
+        <nav
+            className={cn(
+                "mt-auto h-[5.2rem] rounded-t-[.8rem] bg-grey-900 px-[1.6rem] pt-[.8rem] duration-200 md:h-[7.4rem] md:px-[4rem] lg:mt-0 lg:flex lg:h-auto lg:w-[30rem] lg:flex-col lg:gap-[2.4rem] lg:rounded-r-[1.6rem] lg:rounded-tl-none lg:px-0 lg:pb-[5.8rem] lg:pr-[2.4rem] lg:pt-0",
+                {
+                    ["lg:w-[8.8rem] lg:pr-[1rem]"]: !is_open
+                }
+            )}
+        >
+            <div
+                className={cn("hidden px-[3.2rem] py-[4rem] lg:block", {
+                    ["pr-[2.2rem]"]: !is_open
+                })}
+            >
+                <LargeLogo
+                    className={cn(
+                        "inline-block h-[2.2rem] w-[12.1rem] opacity-100 transition-opacity delay-200 duration-200",
+                        {
+                            ["w-0 opacity-0"]: !is_open
+                        }
+                    )}
+                />
+                <SmallLogo
+                    className={cn("hidden h-[2.2rem] w-auto opacity-0", {
+                        ["inline-block opacity-100"]: !is_open
+                    })}
+                />
             </div>
 
             <ul className="flex h-full justify-between lg:h-auto lg:flex-col">
@@ -75,18 +99,23 @@ const NavigationBar = () => {
                         <Link
                             href={link.url}
                             className={cn(
-                                "flex h-full w-full flex-col items-center gap-[.4rem] lg:flex-row lg:gap-[1.6rem] lg:px-[3.2rem] [&>svg]:size-[2.4rem]",
+                                "flex h-full w-full flex-col items-center gap-[.4rem] lg:flex-row lg:gap-[1.6rem] lg:pl-[3.2rem] [&>svg]:size-[2.4rem]",
                                 {
-                                    ["lg:px-[2.8rem]"]: pathname === link.url
+                                    ["lg:pl-[2.8rem]"]: pathname === link.url
                                 }
                             )}
                         >
-                            {link.icon}
+                            <div className="size-[2.4rem] flex-shrink-0 *:size-full">
+                                {link.icon}
+                            </div>
                             <span
                                 className={cn(
                                     "hidden !text-preset_5_bold text-grey-300 group-hover:text-grey-100 md:block lg:!text-preset_3",
                                     {
-                                        "!text-grey-900": pathname === link.url
+                                        "!text-grey-900": pathname === link.url,
+                                        "w-0 opacity-0": !is_open,
+                                        "opacity-100 transition-opacity delay-200 duration-200":
+                                            is_open
                                     }
                                 )}
                             >
@@ -99,10 +128,20 @@ const NavigationBar = () => {
 
             <button
                 type="button"
-                className="mt-auto flex h-[5.6rem] items-center gap-[1.6rem] px-[3.2rem]"
+                className="mt-auto flex h-[5.6rem] items-center gap-[1.6rem] pl-[3.2rem]"
+                onClick={() => setOpen(!is_open)}
             >
-                <BackIcon className="size-[2.4rem]" />
-                <span className="text-preset_3 text-grey-300">
+                <BackIcon
+                    className={cn("size-[2.4rem]", {
+                        ["rotate-180"]: !is_open
+                    })}
+                />
+                <span
+                    className={cn("text-preset_3 text-grey-300", {
+                        ["w-0 opacity-0"]: !is_open,
+                        ["opacity-100 delay-200 duration-200"]: is_open
+                    })}
+                >
                     Minimize Menu
                 </span>
             </button>
