3973d6bfdeb2bd597ae2c543e8e8f7243584189f
Jownsu <jhonesdigno666@gmail.com>
2024-10-29 21:02:15 +0800

    Implement overview balance card

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,28 @@
+/* COMPONENTS */
+import BalanceCard from "../_components/BalanceCard";
+
+/* DATA */
+import { balance } from "../_constants/data";
+
 const HomePage = () => {
     return (
-        <div>
-            <h1 className="text-preset_4_bold">Home Page</h1>
+        <div className="container flex flex-col gap-[3.2rem] py-[2.4rem] md:py-[3.2rem]">
+            <h1 className="text-preset_1">Overview</h1>
+            <div className="flex flex-col gap-[1.2rem] md:flex-row md:gap-[2.4rem]">
+                <BalanceCard
+                    label="Current Balance"
+                    balance={balance.current}
+                    primary
+                />
+                <BalanceCard
+                    label="Current Balance"
+                    balance={balance.current}
+                />
+                <BalanceCard
+                    label="Current Balance"
+                    balance={balance.current}
+                />
+            </div>
         </div>
     );
 };
---
+++ app/_components/BalanceCard.tsx
@@ -0,0 +1,30 @@
+import { cn } from "../_utils/helpers";
+
+interface Props {
+    label: string;
+    balance: number;
+    primary?: boolean;
+}
+
+const BalanceCard = ({ label, balance, primary = false }: Props) => {
+    return (
+        <div
+            className={cn(
+                "flex w-full flex-col gap-[1.2rem] rounded-[1.2rem] bg-white p-[2rem] text-grey-900 md:p-[2.4rem]",
+                {
+                    ["bg-grey-900 text-white"]: primary
+                }
+            )}
+        >
+            <span className="text-preset_4">{label}</span>
+            <span className="text-preset_1">
+                {balance.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD"
+                })}
+            </span>
+        </div>
+    );
+};
+
+export default BalanceCard;
