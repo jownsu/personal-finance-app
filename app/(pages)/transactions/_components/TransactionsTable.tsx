818f82ce6d4115dc5ea29787fd41f5f153dab4e1
Jownsu <jhonesdigno666@gmail.com>
2024-11-24 15:15:29 +0800

    Desktop and tablet of transaction table

--- app/(pages)/transactions/_components/TransactionsTable.tsx
+++ app/(pages)/transactions/_components/TransactionsTable.tsx
@@ -23,18 +23,18 @@ import { transactions } from "@/app/_constants/data";
 const TransactionsTable = () => {
     return (
         <Table>
-            <TableHeader className="hidden">
-                <TableRow>
+            <TableHeader className="hidden md:table-header-group">
+                <TableRow className="text-grey-500 *:px-0 *:py-[2rem] *:text-preset_5 *:xl:py-[1.1rem]">
                     <TableHead>Recipient / Sender</TableHead>
                     <TableHead>Category</TableHead>
                     <TableHead>Transaction Date</TableHead>
-                    <TableHead>Amount</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
                 {transactions.map((transaction, key) => (
-                    <TableRow key={key} className="">
-                        <TableCell className="flex items-center gap-[1.2rem] px-0">
+                    <TableRow key={key} className="*:px-0">
+                        <TableCell className="flex items-center gap-[1.2rem]">
                             <Image
                                 src={transaction.avatar}
                                 alt={`Picture of ${transaction.name}`}
@@ -46,10 +46,16 @@ const TransactionsTable = () => {
                                 <span className="text-preset_4_bold text-gray-900">
                                     {transaction.name}
                                 </span>
-                                <span className="text-preset_5 text-grey-500">
+                                <span className="text-preset_5 text-grey-500 md:hidden">
                                     {transaction.category}
                                 </span>
                             </div>
+                        </TableCell>
+                        <TableCell className="hidden !text-preset_5 text-grey-500 md:table-cell">
+                            {transaction.category}
+                        </TableCell>
+                        <TableCell className="hidden !text-preset_5 text-grey-500 md:table-cell">
+                            {moment(transaction.date).format("D MMM YYYY")}
                         </TableCell>
                         <TableCell className="text-right">
                             <div
@@ -64,8 +70,8 @@ const TransactionsTable = () => {
                                     currency: "USD"
                                 })}
                             </div>
-                            <div className="text-preset_5 text-grey-500">
-                                {moment(transaction.date).format("D MM YYYY")}
+                            <div className="text-preset_5 text-grey-500 md:hidden">
+                                {moment(transaction.date).format("D MMm YYYY")}
                             </div>
                         </TableCell>
                     </TableRow>
--- app/(pages)/transactions/page.tsx
+++ app/(pages)/transactions/page.tsx
@@ -6,7 +6,7 @@ const TransactionPage = () => {
         <div className="container flex flex-col gap-[4rem] py-[2.4rem] md:py-[3.2rem]">
             <h1 className="text-preset_1">Transactions</h1>
 
-            <div className="rounded-[1.2rem] bg-white px-[2rem] py-[2.4rem] md:px-[3.2rem] md:py-[3.2rem]">
+            <div className="flex flex-col gap-[.9rem] rounded-[1.2rem] bg-white px-[2rem] py-[2.4rem] md:gap-[2.5rem] md:px-[3.2rem] md:py-[3.2rem]">
                 <Filters />
                 <TransactionsTable />
             </div>
