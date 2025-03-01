5b6a9d947f0b72efba667161cb2333da0efce59c
Jownsu <jhonesdigno666@gmail.com>
2025-03-01 21:07:36 +0800

    Move to component

---
+++ app/(pages)/bills/_components/RecurringBillsTable.tsx
@@ -0,0 +1,9 @@
+const RecurringBillsTable = () => {
+    return (
+        <div className="h-full w-full rounded-[1.2rem] bg-white p-[3.2rem] lg:flex-1">
+            Table
+        </div>
+    );
+};
+
+export default RecurringBillsTable;
--- app/(pages)/bills/page.tsx
+++ app/(pages)/bills/page.tsx
@@ -1,19 +1,5 @@
-import BillIcon from "@/public/images/icon-recurring-bills.svg";
-
-const SUMMARY = [
-    {
-        label: "Paid Bills",
-        value: "2 ($320.00)"
-    },
-    {
-        label: "Total Upcoming",
-        value: "6 ($1,230.00)"
-    },
-    {
-        label: "Due Soon",
-        value: "2 ($40.00)"
-    }
-];
+import RecurringBillsTable from "./_components/RecurringBillsTable";
+import Summary from "./_components/Summary";
 
 const BillsPage = () => {
     return (
@@ -21,39 +7,8 @@ const BillsPage = () => {
             <h1 className="text-preset_1">Recurring Bills</h1>
 
             <div className="flex h-full flex-col items-start gap-[2.4rem] lg:flex-row">
-                <div className="flex w-full flex-col gap-[1.2rem] md:flex-row md:gap-[2.4rem] lg:basis-[33.7rem] lg:flex-col">
-                    <div className="flex h-[11.8rem] items-center gap-[2.4rem] rounded-[1.2rem] bg-grey-900 px-[2.4rem] text-white md:h-[20.4rem] md:flex-1 md:flex-col md:items-start md:justify-between md:pb-[2.4rem] md:pt-[4.1rem] lg:h-[19rem] lg:flex-none">
-                        <BillIcon className="size-[4rem]" />
-                        <div className="flex flex-col gap-[1.2rem]">
-                            <span className="text-preset_4">Total Bills</span>
-                            <span className="text-preset_1">$384.98</span>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col gap-[.2rem] rounded-[1.2rem] bg-white p-[2rem] md:h-[20.4rem] md:flex-1 lg:flex-none">
-                        <div className="text-preset_3">Summary</div>
-                        <div>
-                            {SUMMARY.map((summary, index) => (
-                                <div
-                                    key={index}
-                                    className="group flex justify-between border-b border-[#69686815] py-[1.6rem] last:border-b-0 last:pb-0"
-                                >
-                                    <span className="text-preset_5 text-grey-500 group-last:text-red">
-                                        {summary.label}
-                                    </span>
-                                    <span className="text-preset_5_bold text-grey-900 group-last:text-red">
-                                        {summary.value}
-                                    </span>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-
-                {/* TABLE */}
-                <div className="h-full w-full rounded-[1.2rem] bg-white p-[3.2rem] lg:flex-1">
-                    Table
-                </div>
+                <Summary />
+                <RecurringBillsTable />
             </div>
         </div>
     );
